6b27fca536b46acc4044ac440628df6ada1f1db2
diogosalvadorb <contact.diogosalvador@gmail.com>
2024-03-02 20:09:08 -0300

    controllers vazias

---
+++ BooksManager.API/Controllers/BooksController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksManager.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+    }
+}
