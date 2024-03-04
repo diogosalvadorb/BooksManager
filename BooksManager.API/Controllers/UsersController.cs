66c7387316b1fd4ea76fc47ac5b681eff0c4ec4e
diogosalvadorb <contact.diogosalvador@gmail.com>
2024-03-03 22:48:08 -0300

    controller user

--- BooksManager.API/Controllers/UsersController.cs
+++ BooksManager.API/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BooksManager.Application.Commands.Users.CreateUser;
+using BooksManager.Application.Queries.Users.GetUserById;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BooksManager.API.Controllers
@@ -7,5 +9,31 @@ namespace BooksManager.API.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly IMediator _mediator;
+        public UsersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserById([FromRoute] int id)
+        {
+            var queryResult = await _mediator.Send(new GetUserByIdQuery(id));
+
+            if(queryResult == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(queryResult);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] CreateUserCommand command)
+        {
+            var id = await _mediator.Send(command);
+
+            return CreatedAtAction(nameof(GetUserById), new { id = id }, command);
+        }
     }
 }
